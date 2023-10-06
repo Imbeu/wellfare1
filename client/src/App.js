0317a232885068cf455cff5f34e3792df4c132b2
Buggy <dbgml1007589@naver.com>
2023-10-07 02:30:20 +0900

    split components and modify design

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// basic
+// import Footer from "./components/base/footer";
+import Header from "./components/base/header";
+
+// main service
+import Home from "./components/home/home";
+import Service from "./components/service/service";
+import Map from "./components/service/map";
+import Favorite from "./components/service/favorite";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/service" element={<Service />}></Route>
+        <Route path="/map" element={<Map />}></Route>
+        <Route path="/favorite" element={<Favorite />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
